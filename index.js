135cbb89f4d60c0c848655b205c17e3e8cc06c3e
Feras <ferasaeoob@gmail.com>
2026-01-28 17:34:41 +0200

    database connection

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+const api = process.env.HOST;
+app.use(express.static(__dirname));
+const db = require('./config/db_config');
+
+app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/login.html')});
+
+app.listen(port,()=>{console.log(`http://${api}:${port}`)});
