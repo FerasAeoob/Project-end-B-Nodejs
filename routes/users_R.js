577166f9a11323f9d9c9e0e4911d581f3b17ed34
Feras <ferasaeoob@gmail.com>
2026-01-29 14:33:36 +0200

    added some function using database and checked

--- routes/users_R.js
+++ routes/users_R.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {getAllUsers} = require('../controller/users_C');
+const {isValidId} = require('../middleware/validate_M');
+const {getOneUser} = require('../controller/users_C');
+
 router.get('/',getAllUsers);
+router.get('/:id',isValidId,getOneUser);
+
 module.exports = router;
